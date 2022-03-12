ff38b92c47220a186ee5ea029cf3ffd3a8f558c1
Sonali <sonali@Sonalis-MacBook-Air.local>
2022-03-12 23:16:58 +0530

    login signup done

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,54 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
 
-router.get('/', async (req,res) => {
+const secret = process.env.SECRET;
+
+router.post('/login', async (req, res) => {
     try {
-        const users = await User.find()
-        res.status(200).json(users)
-    } catch(err) {
-        console.log(err)
-        res.status(400).send({Error: err})
-    }
+        const existingUser = await User.findOne({ email: req.body.email })
+        if(!existingUser) {
+            res.status(200).json({
+                error: true,
+                message: 'User does not exist'
+            })
+        } else {
+            if(req.body.password === existingUser.password) {
+                userDetails = { 
+                    email: existingUser.email, 
+                    password: existingUser.password,
+                    name: existingUser.name,
+                    type: existingUser.type,
+                    phone: existingUser.phone
+                }
+                const token = jwt.sign(userDetails, secret)
+                res.status(200).json({
+                    error: false,
+                    message: 'user token generated',
+                    data: {
+                        token: token,
+                        ...userDetails
+                    }
+                })
+            }
+            else {
+                res.status(200).json({
+                    error: true,
+                    message: 'incorrect password'
+                })
+            } 
+        }
+    } catch (err) {
+        console.log("ERR", err)
+        res.status(500).json({
+            error: true,
+            message: 'Something went wrong'
+        })
+    } 
 })
 
-router.post('/', async (req,res) => {
+router.post('/signup', async (req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -21,11 +57,37 @@ router.post('/', async (req,res) => {
         password: req.body.password
     })
     try {
-        const cuser = await user.save()
-        res.status(200).json(cuser)
+        const existingUser = await User.findOne({ email: req.body.email })
+        if(existingUser) {
+            res.status(200).json({
+                error: true,
+                message: 'User already exists'
+            })
+        } else {
+            const cuser = await user.save()
+            const new_user = {
+                name: cuser.name,
+                email: cuser.email,
+                phone: cuser.phone,
+                type: cuser.type,
+                password: cuser.password
+            }
+            const token = jwt.sign(new_user, secret)
+            res.status(200).json({
+                error: false,
+                message: 'User created successfully',
+                data: {
+                    token: token,
+                    ...new_user
+                }
+            })
+        } 
     } catch(err) {
-        console.log(err)
-        res.status(400).send({Error: err})
+        console.log("ERR", err)
+        res.status(500).json({
+            error: true,
+            message: 'Something went wrong'
+        })
     }
 })
 
